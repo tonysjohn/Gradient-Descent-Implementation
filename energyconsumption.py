85472b8ee817e952a17a78bb0675ef50ae7c4e15
tonysjohn <tonysjohn@github.com>
2019-09-06 01:42:47 -0500

    added predict functions

--- energyconsumption.py
+++ energyconsumption.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from gradientDescend import *
+from gradientDescend import lm,logit,predict
 
 
 ###################### reading data #######################################
@@ -10,11 +10,18 @@ y=df["Appliances"]
 x=df[['T1','T2','T3','T4','T5','T6']]
 x=np.array(x)
 x_norm=(x-x.mean(axis=0))/x.std(axis=0)
-x_norm
+x_norm.shape
+y
 y=np.array(y).reshape(-1,1)
 
+y_class = np.where(y>np.median(y),1,0)
 
-lm(X=x_norm, Y=y, verbose=False, maxiter=100000, alpha =0.1, restart=1, tol= 1e-8)
+y_class
+
+predict(logit, x_norm,y_class, x_norm,y_class, verbose=False, maxiter=100000, alpha =0.01, restart=1, tol= 1e-8)
+
+logistic(x_norm,y_class, verbose=False, maxiter=100000, alpha =0.01, restart=1, tol= 1e-8)
+
 
 a=[]
 c=[]
@@ -35,8 +42,12 @@ plt.plot(a, c, 'o')
 df.describe()
 
 import statsmodels.api as sm
+import statsmodels.discrete.discrete_model as smd
 
 mod = sm.OLS(y, x_norm)
 res = mod.fit()
 res.summary()
+
+mod1=smd.Logit(y_class, x_norm).fit()
+mod1.summary()
 
--- gradientDescend.py
+++ gradientDescend.py
@@ -4,38 +4,76 @@ import pandas as pd
 ################## initializing beta ####################################
 
 def initbeta(n):
-    b = np.random.rand()
-    W = np.random.rand(n,1)
+    """
+    input:  n - number of features
+    output: b - beta0
+            W - beta
+    weights assigned to near zero so that logistic regression converges faster
+    """
+    b = np.random.rand()*0.01
+    W = np.random.rand(n,1)*0.01
     return (b,W)
 
-################## computing cost and gradient ##########################
+################## computing OLS cost and gradient ######################
 
-def rmse(X,y,b=0,W=0):
+def ols(X,y,b=0,W=0):
+    m,n = X.shape
+    ypred, cost = predictlm(X,y,b,W)
+    err= y - ypred
+    db = -(1/m)*np.sum(err)
+    dW = -(1/m)*X.T.dot(err)
+    return (cost, db, dW)
+
+####################### predict lm #####################################
+
+def predictlm(X,y,b,W):
     m,n = X.shape
     ypred = np.dot(X,W) + b
     err = (y - ypred)
-    # cost = 1/(2*m)*(err.dot(err.T))
     cost = (1/(2*m)) * np.sum(np.square(err))
+    return (ypred, cost)
+
+################## computing MLE cost and gradient ######################
+
+def mle(X,y,b=0,W=0):
+    m,n = X.shape
+    ypred, cost = predictlogit(X,y,b,W)
+    err= y - ypred
     db = -np.sum(err)/float(m)
-    # dW = 1/m*((err*x).sum(axis=1)).reshape(-1,1)
-    # print(err.shape,X.shape)
     dW = -(1/m)*X.T.dot(err)
-    # dW = -(1/m)*np.sum(np.multiply(X,err), axis=0)
     return (cost, db, dW)
+
+####################### predict logit ##################################
+
+def predictlogit(X,y,b,W):
+    m,n = X.shape
+    z = np.dot(X,W) + b
+    ypred = 1/(1+np.exp(-z))
+    err = (y - ypred)
+    cost = -(1/m) * (y.T.dot(np.log(ypred)).reshape(-1)+(1-y).T.dot(np.log(1-ypred)).reshape(-1))
+    return (ypred, cost)
+
+######################### selecting method #############################
+
+def method(X,y,b,W,mth):
+    if mth == "lm":
+        return ols(X,y,b,W)
+    elif mth == "binary":
+        return mle(X,y,b,W)
 
 
 ##################### gradient descent #################################
 
-def descend(X, y, alpha=0.001, verbose=True, maxiter=100, tol=1e-6):
+def descend(X, y, mth, alpha=0.001, verbose=True, maxiter=10000, tol=1e-6):
     m,n = X.shape
     b,W = initbeta(n)
     oldcost,i = np.inf,0
-    cost, db, dW = rmse(X,y,b,W)
+    cost, db, dW = method(X,y,b,W,mth)
     b = b - alpha*db
     W = W - alpha*dW
     while (oldcost - cost > tol):
         oldcost = cost
-        cost, db, dW = rmse(X,y,b,W)
+        cost, db, dW = method(X,y,b,W,mth)
 
         b = b- alpha*db
         W = W- alpha*dW
@@ -50,22 +88,55 @@ def descend(X, y, alpha=0.001, verbose=True, maxiter=100, tol=1e-6):
     if oldcost < cost:
         print('Decrease alpha : solution not converging')
         return (np.nan,cost)
-    elif i < maxiter:
+    if i < maxiter:
         print('Solution found in {}th iteration'.format(i))   
-    return (np.append(b,W),cost)
+    return (b,W.reshape(-1,1),cost)
 
 
 #################### LM wrapper and random restart #######################
 
-def lm(X, y, alpha=0.001, verbose=True, maxiter=100, restart=1, tol = 1e-6):
+def lm(X, y, alpha=0.001, verbose=True, maxiter=10000, restart=1, tol = 1e-6):
     mincost=np.inf
-    
     for i in range(restart):
-        beta, cost = descend(X, y, alpha=alpha, verbose=verbose,maxiter=maxiter, tol=tol)
+        b,W, cost = descend(X, y, mth="lm", alpha=alpha, verbose=verbose,maxiter=maxiter, tol=tol)
         if cost < mincost:
-            # print('{0:0.2f} at {1}th iteration'.format(cost,i+1))
-            finalbeta = beta
+            finalb = b
+            finalW = W
             mincost = cost
         else:
             continue
-    return finalbeta, mincost
+    return finalb, finalW, mincost
+
+#################### Logistic wrapper and random restart #######################
+
+def logit(X, y, alpha=0.001, verbose=True, maxiter=10000, restart=1, tol = 1e-6):
+    mincost=np.inf
+    for i in range(restart):
+        b,W, cost = descend(X, y, mth="binary", alpha=alpha, verbose=verbose,maxiter=maxiter, tol=tol)
+        if cost < mincost:
+            finalb = b
+            finalW = W
+            mincost = cost
+        else:
+            continue
+    return finalb, finalW, mincost
+
+########################### Predict function ################################
+
+
+def predict(method, X, y, newX=None, newy=None, alpha=0.001, verbose=True, maxiter=10000, restart=1, tol = 1e-6):
+    if method not in [lm,logit]:
+        print("Please provide a method")
+        return None
+    b, W, cost = method(X, y, alpha=alpha, verbose=verbose, maxiter=maxiter, restart=restart, tol = tol)
+    if newX is None:
+        if method == lm:
+            return (predictlm(X,y,b,W), None)
+        elif method == logit:
+            return (predictlogit(X,y,b,W), None)
+    else:
+        if method == lm:
+            return (predictlm(X,y,b,W), predictlm(newX,newy,b,W))
+        elif method == logit:
+            return (predictlogit(X,y,b,W), predictlogit(newX,newy,b,W))
+
